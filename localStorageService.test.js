cabb123a89d8b0162ac0cb16d982408aea63bd50
Safir Manghat <safir.manghat@utb.tyreso.se>
2025-03-26 11:18:32 +0100

    loca storage test

--- localStorageService.test.js
+++ localStorageService.test.js
@@ -4,14 +4,17 @@ const localStorageMock = {
     clear: jest.fn()
   };
 it ('should set item in local storage', () => {
-    localStorageMock.setItem('key', 'value');
-    expect(localStorageMock.setItem).toHaveBeenLastCalledWith('key', 'value');});
+    localStorageMock.setItem('2', 'python');
+    expect(localStorageMock.setItem).toHaveBeenLastCalledWith('2','python');});
 it ('should get item from local storage', () => {
-    localStorageMock.getItem('key');
-    expect(localStorageMock.getItem).toHaveBeenLastCalledWith('key');});
+    localStorageMock.getItem('2');
+    expect(localStorageMock.getItem).toHaveBeenLastCalledWith('2');});
 it ('should clear local storage', () => {   
     localStorageMock.clear();
     expect(localStorageMock.clear).toHaveBeenCalled();});
+it('should return java', () => {
+    localStorageMock.getItem('1');
+    expect(localStorageMock.getItem).toHaveBeenLastCalledWith('1');});
 
 
   global.localStorage = localStorageMock;
